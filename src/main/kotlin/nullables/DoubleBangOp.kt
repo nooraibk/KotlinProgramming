43f0ffcec46ea8c991fbfd8f65a3798ec667332e
nooraibk <subject-works.culvert@aleeas.com>
2024-10-01 08:33:34 +0500

    cleaning iteration third

--- src/main/kotlin/nullables/DoubleBangOp.kt
+++ src/main/kotlin/nullables/DoubleBangOp.kt
@@ -1,3 +1,5 @@
+package nullables
+
 fun main(){
 
     //try commenting and uncommenting both scenarios
@@ -7,5 +9,17 @@ fun main(){
 //    val b = a?.length //putting ? sign will not crash the code even if the value is null, rather it will return the null value
 
     val b = a!!.length //however !! operator can crash the code so it should only be used when you are 100% sure that the value is not null and you just want to bypass kotlin compiler check for null safety
+
+    var xyz = 9
+    if (NullableSingleton.abc != null){
+        xyz = NullableSingleton.abc!! //abc is nullable while xyz is not
+    }
     println(b)
+}
+
+object NullableSingleton {
+    var abc: Int? = null
+    fun nullsAnObj(){
+        abc = 99
+    }
 }
--- src/main/kotlin/nullables/ElsvinOperator.kt
+++ src/main/kotlin/nullables/ElsvinOperator.kt
@@ -1,3 +1,5 @@
+package nullables
+
 fun main(){
     /**
      * Elvis Operator ?:
