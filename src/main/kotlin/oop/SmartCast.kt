8486b313e8b86d530ea790f8c22d456ad37e1a73
nooraibk <subject-works.culvert@aleeas.com>
2024-10-10 08:23:57 +0500

    smart cast

--- src/main/kotlin/oop/SmartCast.kt
+++ src/main/kotlin/oop/SmartCast.kt
@@ -1,5 +1,18 @@
 package oop
 
+
+/**
+### Limitations
+
+Smart casting works under certain conditions:
+
+- The type check must be done in a straightforward way (using is).
+- The variable must not be modified after the type check (e.g., reassigned or changed).
+- It works with immutable variables (declared with val). If a variable is mutable (declared with var), the compiler cannot guarantee that its type hasn't changed after the check.
+*/
+
+
+
 open class Animal {
     open fun makeSound() {
         println("Some generic animal sound")
@@ -7,14 +20,29 @@ open class Animal {
 }
 
 class Dog : Animal() {
-
     override fun makeSound() {
         println("Woof! Woof!")
     }
 }
 
-fun handleAnimal(animal: Animal) {
-    if (animal is Dog) {
+class Cat : Animal() {
+    override fun makeSound() {
+        println("Meow! Meow!")
+    }
+}
+
+fun handleAnimal(temp : Int) {
+    var animal = Animal()
+
+    if (temp == 0){
+        animal = Dog()
+    }
+
+    if (temp > 0){
+        animal = Cat()
+    }
+
+    if (animal is Cat) {
         // Smart cast: 'animal' is automatically cast to Dog within this block
         animal.makeSound() // This is valid because 'animal' is now treated as a Dog
     }
@@ -40,6 +68,15 @@ fun describe(obj: Any) {
 }
 
 fun main() {
-    val myDog: Animal = Dog()
-    handleAnimal(myDog)
+    handleAnimal(1)
+
+
+    var x: String? = "Hello"
+    if (x != null) {
+        println(x.length) // This works
+        println(x.uppercase()) // This also works
+        x = null
+        println(x.length) // Error: x is nullable and is not checked for null
+    }
+
 }
